6a5e06d8bb1164919c210520ff03f0bf14dc5a8c
Francisco Escobedo <fescobedo@mscdirect.com.mx>
2024-05-10 13:35:05 -0600

    Se termina con la primera parte del curso y empezamos con Jquery

---
+++ js/37-try&catch.js
@@ -0,0 +1,9 @@
+
+try {
+    var year = 2024;
+    alert(year);
+    console.log(decodeURIComponent("https://v&&ictorroblesweb.es"));
+} catch (error) {
+    console.log(error)
+    alert("Ha ocurrido un error en el código");
+} // console.log(year);     // Error
